6bb10560c341b3c13ed063f619ffb762ea200fae
Palak Jain <73011168+palakjain25@users.noreply.github.com>
2020-12-18 01:28:43 +0530

    Create Q1.kt

---
+++ week-1/Q1.kt
@@ -0,0 +1,9 @@
+//to check if the given number is positive or negative
+fun main() 
+{    
+  val a: Int = -5  
+  if(a>0) 
+    println("$a is positive")
+  else 
+    println("$a is negative")     
+}
