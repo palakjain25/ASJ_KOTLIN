9ed8a5480d2dbe0972c5806d458299f1265ebc21
Palak Jain <73011168+palakjain25@users.noreply.github.com>
2020-12-18 19:04:54 +0530

    Create Q10.kt

---
+++ week-1/Q10.kt
@@ -0,0 +1,11 @@
+//to print a half pyramid of given height
+fun main() 
+{
+    val x: Int = 4
+    for(i in 1..x)
+    {        
+        for(j in 1..i)
+        print("#")
+        println()
+    }              
+}
