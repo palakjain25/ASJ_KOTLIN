6690c383e21b68b089a75da983c2b45967549979
Palak Jain <73011168+palakjain25@users.noreply.github.com>
2020-12-20 21:38:17 +0530

    Create Q12.kt

---
+++ week-1/Q12.kt
@@ -0,0 +1,27 @@
+//to check if a given n digit number is an armstrong number or not
+import kotlin.math.pow
+fun main()
+{
+    val x = 153
+    var temp: Int
+    var p = 0
+    var remainder: Int
+    var sum = 0    
+    temp = x
+    while (temp != 0)
+    {
+        temp /= 10
+        ++p
+    }
+    temp = x
+    while (temp != 0)
+    {
+        remainder = temp % 10
+        sum += Math.pow(remainder.toDouble(), p.toDouble()).toInt()
+        temp /= 10
+    }
+    if (sum == x)
+        println("$x is an Armstrong number.")
+    else
+        println("$x is not an Armstrong number.")
+}
